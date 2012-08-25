b287ad89484a2738d0b2292da081d716f1cd5678
aurelianonava <sersinfin@gmail.com>
2012-08-25 09:56:57 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApushCards::Application.config.secret_token = 'e40f8fa39935f1f0020b5eb72102a4e25764c55e50b75471b4004a6d5b6169b1e6b6e499bd599124ee52b919faa006c22250ca90bb786886bbd933b308ccff11'
